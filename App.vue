491c96d7d28a8efb90d735ed66ee1329feaedd62
zhengzhiyu <zhengzhiyu825@163.com>
2020-04-29 14:48:43 +0800

    45

---
+++ App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        father
+        {{ msg }}
+        <p>我是父组件p元素</p>
+        <base-son/>
+    </div>
+</template>
+
+<script>
+import baseSon from './Son'
+
+export default {
+    components: {
+        baseSon
+    },
+    data () {
+        return {
+            msg: 'hello vue'
+        }
+    }
+}
+</script>
+
+<style>
+    div {
+        background-color: #f00;
+    }
+</style>
---
+++ Son.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        son
+        <p>我是子组件p元素</p>
+    </div>
+</template>
+
+<style scoped>
+    p {
+        background-color: #0f0;
+    }
+</style>
