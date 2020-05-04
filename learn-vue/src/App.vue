09129f6c7689830d07fa1d0738afb0a50437725f
zhengzhiyu <zhengzhiyu825@163.com>
2020-05-04 10:42:05 +0800

    51

--- learn-vue/src/App.vue
+++ learn-vue/src/App.vue
@@ -1,28 +1,50 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
+
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+// <template>
+//   <div id="app" class="app">
+//     <!-- <base-level :level="1">标题</base-level>
+//     <base-level :level="2">标题</base-level>
+//     <base-level :level="3">标题</base-level>
+//     <base-level :level="4">标题</base-level>
+//     <base-level :level="5">标题</base-level>
+//     <base-level :level="6">标题</base-level> -->
+//     <!-- <base-data></base-data> -->
+
+//     <base-slots v-slot="slotProps">
+//       {{slotProps.text}}
+//     </base-slots>
+//   </div>
+// </template>
+
+import BaseLevel from './components/BaseLevel';
+import BaseData from './components/BaseData';
+import BaseSlots from './components/BaseSlots';
+
+
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
-  }
+    BaseLevel,
+    BaseData,
+    BaseSlots
+  },
+  render (h) {
+    return h('div', {
+      class: 'app',
+      id: 'app',
+    }, [
+      h('base-slots', {
+        scopedSlots: {
+          default: function (props) {
+            return props.text;
+          }
+        },
+      })
+    ])
+  },
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
