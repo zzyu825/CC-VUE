95b7b79fd1d2fdeda70188cc62119bd780c22e90
zhengzhiyu <zhengzhiyu825@163.com>
2020-07-08 17:31:13 +0800

    59

--- learn-vue/src/App.vue
+++ learn-vue/src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <button @click="show = true">click</button>
-    <async-cmp-1 v-if="show" />
-    <async-cmp-2 v-if="show" />
 
+    <div class="nav-box">
+      <div class="logo">渡一教育</div>
+      <div class="nav-list">
+        <router-link to="/">首页</router-link>
+        <router-link to="/learn">课程学习</router-link>
+        <router-link to="/student">学员展示</router-link>
+        <router-link to="/about">关于</router-link>
+        <router-link to="/activity">社区</router-link>
+      </div>
+    </div>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-// import AsyncCmp from './components/AsyncCmp';
-
-// import('')
 
 export default {
   name: 'app',
   components: {
-    AsyncCmp1: () => import(/* webpackChunkName: "async" */  './components/AsyncCmp1'),
-    AsyncCmp2: () => import(/* webpackChunkName: "async" */  './components/AsyncCmp2'),
-
-  },
-  data () {
-    return {
-      show: false,
-    }
   },
 }
 </script>
+
+<style scoped>
+.nav-box {
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  line-height: 60px;
+  background-color: #3385ff
+}
+
+.nav-box .logo {
+  color: #fff;
+}
+
+.nav-list a {
+  margin-left: 40px;
+  color: #fff;
+  text-decoration: none;
+}
+
+.nav-list a.router-link-exact-active {
+  font-weight: bold;
+}
+
+.container {
+  margin-top: 60px;
+}
+
+.nav-box,
+.container {
+  padding-left: 200px;
+  padding-right: 200px;
+}
+</style>
--- learn-vue/src/components/AsyncCmp1.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="async">
-    我是一个不需要再首屏首次加载出来的组件1
-  </div>
-</template>
