4b9cfb2970310c79ce11055f26e03c8db4a4e2e9
zhengzhiyu <zhengzhiyu825@163.com>
2020-07-11 14:06:54 +0800

    change

--- learn-vue/src/App.vue
+++ learn-vue/src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
 
     <div class="nav-box">
-      <div class="logo">渡一教育</div>
+      <div class="logo">CC教育</div>
       <div class="nav-list">
         <router-link to="/home">首页</router-link>
         <router-link to="/learn">课程学习</router-link>
