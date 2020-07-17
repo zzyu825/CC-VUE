77dbe9f90d402fcb071f21d67616f8bf4a92e04b
zhengzhiyu <zhengzhiyu825@163.com>
2020-07-17 15:18:45 +0800

    replace

---
+++ learn-vue/src/views/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login">
+        <button @click="handel">{{ btnText }}</button>
+    </div>
+</template>
+
+<script>
+import Login from '../utils/login'
+export default {
+    data() {
+        return {
+            isLogin: Login.isLogin()
+        }
+    },
+    methods: {
+        handel() {
+            if (this.isLogin) {
+                login.cancelLogin();
+            } else {
+                Login.login();
+                this.goBack();
+            }
+            this.isLogin = !this.isLogin;
+        },
+        goBack() {
+            const flag = window.confirm('登录成功，是否要回到原来的页面？');
+            if (flag) {
+                this.$router.go(-1);
+            }
+        }
+    },
+    computed: {
+        btnText() {
+            return this.isLogin ? '取消登录' : '登录'
+        }
+    }
+}
+</script>
